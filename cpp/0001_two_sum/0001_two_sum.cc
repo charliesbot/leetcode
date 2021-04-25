0b4acbb80f479c585b1b8d10d783dd3fa926dc9f
Charlie L <charliesbot@gmail.com>
2021-04-25 16:52:33 -0700

    add first exercise with cpp

--- cpp/0001_two_sum/0001_two_sum.cc
+++ cpp/0001_two_sum/0001_two_sum.cc
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
+
 using std::cout;
+using std::unordered_map;
+using std::vector;
+
+class Solution
+{
+public:
+  vector<int> twoSum(vector<int> &nums, int target)
+  {
+    unordered_map<int, int> umap;
+    vector<int> result = {};
+    for (int i = 0; i < nums.size(); i++)
+    {
+      const int current = nums[i];
+
+      if (umap.count(current) > 0)
+      {
+        result = {i, umap[current]};
+        break;
+      }
+
+      const int diff = target - nums[i];
+      umap[diff] = i;
+    }
+
+    return result;
+  }
+};
 
 int main()
 {
-  cout << "hola";
+  vector<int> nums = {2, 7, 11, 15};
+  auto result = Solution().twoSum(nums, 9);
+  for (auto n : result)
+  {
+    cout << n << " ";
+  }
 }
