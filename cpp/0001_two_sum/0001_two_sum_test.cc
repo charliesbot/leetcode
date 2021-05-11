d9568fa04adc33aa87a12c451f7a24ae6064a025
Charlie L <charliesbot@gmail.com>
2021-05-10 20:39:27 -0700

    fix monorepo

--- cpp/0001_two_sum/0001_two_sum_test.cc
+++ cpp/0001_two_sum/0001_two_sum_test.cc
@@ -1,11 +1,10 @@
-#include "cpp/0001_two_sum.h"
+#include "cpp/0001_two_sum/0001_two_sum.h"
 #include <gtest/gtest.h>
 #include <vector>
 
 using std::vector;
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(TwoSumTest, Example1) {
   vector<int> nums = {2, 7, 11, 15};
   const vector<int> mock = Solution().twoSum(nums, 9);
 
