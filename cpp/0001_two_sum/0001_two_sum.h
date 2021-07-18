3f83bff6996d071ceebb6b2a5dd6f858792c4a33
Charlie L <charliesbot@gmail.com>
2021-07-17 22:29:37 -0700

    quick fix to address warnings

--- cpp/0001_two_sum/0001_two_sum.h
+++ cpp/0001_two_sum/0001_two_sum.h
@@ -7,13 +7,12 @@ using std::vector;
 class Solution {
 public:
   vector<int> twoSum(vector<int> &nums, int target) {
-
     unordered_map<int, int> umap;
-    for (int i = 0; i < nums.size(); i++) {
+    for (uint i = 0; i < nums.size(); i++) {
       const int current = nums[i];
 
       if (umap.count(current) > 0) {
-        return {i, umap[current]};
+        return {(int)i, umap[current]};
       }
 
       const int diff = target - nums[i];
